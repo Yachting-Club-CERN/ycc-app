d3d9c3714aeafda3cda776b09f9fd4c25a260aa4
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-04-02 18:01:07 +0200

    Minor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <ReadingFriendlyBox>
       <>
-        <PageTitle value="Welcome to the new & fancy YCC app! 🎉" />
+        <PageTitle value="Welcome to the new & fancy YCC App! 🎉" />
         <SpacedTypography>
           Work is in progress... 🚧 Hopefully over time we get more and more
           features here! We really hope that your 📱 will like it too! In the
@@ -22,14 +22,14 @@ const Home = () => {
         <SpacedTypography>
           We need Helpers for various tasks, such as design (drawing on a piece
           of paper is perfect), testing (touching on a phone, clicking on a
-          computer).
+          computer) - no computer experience needed!
         </SpacedTypography>
         <SpacedTypography>
-          For nerds it is a lifetime opportunity to practice or simply get into
-          TypeScript, React, MUI, Python, FastAPI, SQLAlchemy, Keycloak, Java,
-          Oracle DB, Openshift (... of course no need for all the same time ...
-          and whatever you would like, infra is ready to create standalone
-          apps). We are on{' '}
+          For nerds it is a lifetime opportunity to practise or simply get into
+          TypeScript, React, MUI, Python, FastAPI, SQLAlchemy, Keycloak, OKD,
+          Java, Oracle DB (... of course no need for all the same time ... and
+          whatever you would like, infra is ready to create standalone apps). We
+          are on{' '}
           <Link href={externalUrls.yccGithub} target="_blank" rel="noopener">
             GitHub
           </Link>
