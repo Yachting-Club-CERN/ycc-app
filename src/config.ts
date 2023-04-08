fc57451cad10fb3812810fb330f9691a76bbe833
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-04-09 00:23:20 +0200

    Add DEV with local YCC Hull config

--- src/config.ts
+++ src/config.ts
@@ -13,29 +13,45 @@ type Config = {
   yccHullUrl: string;
 };
 
-const DEFAULT_LOCAL_CONFIG: Config = {
-  environment: Environment.LOCAL,
-  keycloakServerUrl: 'http://localhost:8080/',
-  keycloakRealm: 'YCC-LOCAL',
-  keycloakClient: 'ycc-app-local',
+// Use this if you run everything locally
+//
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const LOCAL_CONFIG: Config = {
+  environment: Environment.DEVELOPMENT,
+  keycloakServerUrl: 'https://ycc-auth.web.cern.ch/',
+  keycloakRealm: 'YCC-DEV',
+  keycloakClient: 'ycc-app-dev',
   yccHullUrl: 'http://localhost:8000',
 };
+
+// Use this configuration if you run the backend locally, but you use KeyCloak YCC-DEV from CERN OKD
+//
+// Note: if you run YCC Hull locally, make sure that it connects to the same database at CERN as Keycloak realm YCC-DEV
+//
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const DEV_WITH_LOCAL_HULL_CONFIG: Config = {
+  environment: Environment.DEVELOPMENT,
+  keycloakServerUrl: 'https://ycc-auth.web.cern.ch/',
+  keycloakRealm: 'YCC-DEV',
+  keycloakClient: 'ycc-app-dev',
+  yccHullUrl: 'http://localhost:8000',
+};
+
+const DEFAULT_CONFIG = LOCAL_CONFIG;
 
 const config: Config = {
   environment:
     Environment[
       process.env.REACT_APP_ENVIRONMENT as keyof typeof Environment
-    ] || DEFAULT_LOCAL_CONFIG.environment,
+    ] || DEFAULT_CONFIG.environment,
   keycloakServerUrl:
     process.env.REACT_APP_KEYCLOAK_SERVER_URL ||
-    DEFAULT_LOCAL_CONFIG.keycloakServerUrl,
+    DEFAULT_CONFIG.keycloakServerUrl,
   keycloakRealm:
-    process.env.REACT_APP_KEYCLOAK_REALM || DEFAULT_LOCAL_CONFIG.keycloakRealm,
+    process.env.REACT_APP_KEYCLOAK_REALM || DEFAULT_CONFIG.keycloakRealm,
   keycloakClient:
-    process.env.REACT_APP_KEYCLOAK_CLIENT ||
-    DEFAULT_LOCAL_CONFIG.keycloakClient,
-  yccHullUrl:
-    process.env.REACT_APP_YCC_HULL_URL || DEFAULT_LOCAL_CONFIG.yccHullUrl,
+    process.env.REACT_APP_KEYCLOAK_CLIENT || DEFAULT_CONFIG.keycloakClient,
+  yccHullUrl: process.env.REACT_APP_YCC_HULL_URL || DEFAULT_CONFIG.yccHullUrl,
 };
 
 export {Environment};
