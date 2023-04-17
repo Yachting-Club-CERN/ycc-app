2cf0f54f9544287265a51818e632c0b3e9f908d4
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-04-18 00:01:50 +0200

    Add TinyMCE

---
+++ postinstall.js
@@ -0,0 +1,38 @@
+// See https://www.tiny.cloud/docs/tinymce/6/react-pm-host/
+
+const fse = require('fs-extra');
+const path = require('path');
+
+// Realpath for pnpm links
+source = fse.realpathSync(path.join(__dirname, 'node_modules', 'tinymce'));
+target = path.join(__dirname, 'public', 'tinymce');
+
+const shouldCopy = file => {
+  if (file.endsWith('.js')) {
+    return file.endsWith('.min.js');
+  } else if (file.endsWith('.css')) {
+    return file.endsWith('.min.css');
+  } else {
+    return true;
+  }
+};
+
+const copySubdir = file =>
+  fse.copySync(path.join(source, file), path.join(target, file), {
+    overwrite: true,
+    filter: file => shouldCopy(file),
+  });
+
+fse.emptyDirSync(target);
+[
+  'icons',
+  'models',
+  'plugins',
+  'skins',
+  'themes',
+  'license.txt',
+  'tinymce.min.js',
+].forEach(copySubdir);
+
+// Full copy would be:
+// fse.copySync(source, target, {overwrite: true});
